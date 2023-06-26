3e34ca47a7ae434eda75790badee5d4089814086
Villa25698 <bardha@hiof.no>
2023-06-26 21:58:31 +0200

    progresjon

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,41 @@
+<template>
+    <footer class="text-center bg-light pt-4">
+      <!-- Social media links -->
+      <div class="mb-4">
+        <a class="btn btn-link btn-lg text-dark m-1" href="#" role="button">
+          <i class="fab fa-facebook-f"></i>
+        </a>
+        <a class="btn btn-link btn-lg text-dark m-1" href="#" role="button">
+          <i class="fab fa-twitter"></i>
+        </a>
+        <a class="btn btn-link btn-lg text-dark m-1" href="#" role="button">
+          <i class="fab fa-google"></i>
+        </a>
+        <a class="btn btn-link btn-lg text-dark m-1" href="#" role="button">
+          <i class="fab fa-instagram"></i>
+        </a>
+        <a class="btn btn-link btn-lg text-dark m-1" href="#" role="button">
+          <i class="fab fa-linkedin"></i>
+        </a>
+        <a class="btn btn-link btn-lg text-dark m-1" href="#" role="button">
+          <i class="fab fa-github"></i>
+        </a>
+      </div>
+  
+      <!-- Copyright -->
+      <div class="text-center text-dark p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2023 Copyright: <a class="text-dark" href="https://yourwebsite.com/">YourWebsite.com</a>
+      </div>
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+    name: 'AppFooter'
+  }
+  </script>
+  
+  <style scoped>
+  /* CSS styles go here */
+  </style>
+  
