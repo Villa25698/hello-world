3d9d50dc8b4480b5ef9be1b5e7be8cdef09c32ee
Villa25698 <bardha@hiof.no>
2023-06-26 22:23:09 +0200

    glemte import :p

--- src/main.js
+++ src/main.js
@@ -5,6 +5,6 @@ import router from './router'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
+import 'bootstrap/dist/js/bootstrap.bundle'
 
 createApp(App).use(store).use(router).mount('#app')
