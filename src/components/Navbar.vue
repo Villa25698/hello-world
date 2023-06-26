1e1d832c22ebdd471021057f1e1e260ea619fa2e
Villa25698 <bardha@hiof.no>
2023-06-26 13:49:59 +0200

    test 1

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+      <router-link to="/contact">Contact</router-link>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Navbar'
+  }
+  </script>
+  
+  <style scoped>
+  nav {
+    /* Add your styles here */
+  }
+  </style>
+  
